7720678ed3481ba2dcd4087621299b8013120cb5
shassan08 <sajahassan0111@gmail.com>
2024-06-21 19:58:05 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+    for (let i = 100; i < canvas.width; i += 100) {
+      createPlatform(i, canvas.height, -1, -canvas.height);
+    }
+    for (let i = 100; i < canvas.height; i += 100) {
+      createPlatform(canvas.width, i, -canvas.width, -1);
+    }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -43,18 +43,18 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-createPlatform(550, 300, 200, 20, 8); // short but wide platform located 550 pixels from the left of the screen and 300 pixels from the top of the screen
-
-
+createPlatform(500, 425, 300, 20, 8); // short but wide platform located 550 pixels from the left of the screen and 300 pixels from the top of the screen
+createPlatform(650, 540, 300, 30, 10); // short but wide platform located 650 pixels from the left of the screen and 400 pixels from the top of the screen 
+createPlatform(300, 640, 300, 20, 8); // short but wide platofrm located 300 pixels from the left of the screen and 600 pixels from the top of the screen
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-createCollectable("maple", 550, 300, 20, 0.5);//creates a "maple" collectible at the coordinates (550, 300), falling with a high gravity of 20, and bouncing with 50% bounce
-createCollectable("bob", 600, 400, 30, 0.7);//creates a "bob" collectible at the coordinates (600, 400), falling with a high gravity of 30 and bouncing with 70% bounce
-createCollectable("sam", 800, 300); // creates a "sam" collectible at the coordinates (800, 300), falling with default gravity and bouncing with default bounce %
+createCollectable("database", 550, 300, 20, 0.5);//creates a "maple" collectible at the coordinates (550, 300), falling with a high gravity of 20, and bouncing with 50% bounce
+createCollectable("diamond", 600, 400, 30, 0.7);//creates a "bob" collectible at the coordinates (600, 400), falling with a high gravity of 30 and bouncing with 70% bounce
+createCollectable("grace", 800, 300); // creates a "sam" collectible at the coordinates (800, 300), falling with default gravity and bouncing with default bounce %
 
 
     // TODO 3
@@ -62,12 +62,10 @@ createCollectable("sam", 800, 300); // creates a "sam" collectible at the coordi
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-createCannon("right", 500, 1000); //cannon on right wall, 500px down, shooting once per second
-createCannon("top", 600, 1000); //cannon on top wall, 600 px down, shooting once per second
-createCannon("right", 800, 1000); //cannon on right wall, 800px down, shooting once per second
-createCannon("left", 700, 1000); //cannon on left wall, 700px down, shooting once per second
-
-
+    createCannon("right", 500, 2000); //cannon on right wall, 500px down, shooting once per 7 second
+    createCannon("top", 600, 2000); //cannon on top wall, 600 px down, shooting once per  8 seconds
+    createCannon("right", 800, 2000); //cannon on right wall, 800px down, shooting once per 5 seconds
+    
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
